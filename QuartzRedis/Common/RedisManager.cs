09a7e8fee5215f40b63301e0babcb31fdd50d5b5
Mitoriko <mitoriko@icloud.com>
2019-04-09 18:14:48 +0800

    完成配合测试代码

---
+++ QuartzRedis/Common/RedisManager.cs
@@ -0,0 +1,31 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuartzRedis.Common
+{
+    public class RedisManager
+    {
+        private static ConfigurationOptions connDCS = ConfigurationOptions.Parse("www.a-cubic.com:16379");
+
+        private static readonly object Locker = new object();
+
+        private static ConnectionMultiplexer redisConn;
+
+        public static ConnectionMultiplexer getRedisConn()
+        {
+            if (redisConn == null)
+            {
+                lock (Locker)
+                {
+                    if (redisConn == null || !redisConn.IsConnected)
+                    {
+                        redisConn = ConnectionMultiplexer.Connect(connDCS);
+                    }
+                }
+            }
+            return redisConn;
+        }
+    }
+}
---
+++ QuartzRedis/Program.cs
@@ -0,0 +1,72 @@
+﻿using Quartz;
+using Quartz.Impl;
+using QuartzRedis.Common;
+using StackExchange.Redis;
+using System;
+using System.Collections.Specialized;
+using System.Threading.Tasks;
+
+namespace QuartzRedis
+{
+    class Program
+    {
+        //public static void onMessageHandle(ChannelMessage channelMessage)
+        //{
+        //    Console.WriteLine(channelMessage.Message.ToString());
+        //}
+        //static Action<ChannelMessage> action = new Action<ChannelMessage>(onMessageHandle);
+
+        static void Main(string[] args)
+        {
+            StartAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task StartAsync()
+        {
+            // construct a scheduler factory
+            NameValueCollection props = new NameValueCollection
+            {
+                { "quartz.serializer.type", "binary" }
+            };
+            StdSchedulerFactory factory = new StdSchedulerFactory(props);
+
+            // get a scheduler
+            IScheduler sched = await factory.GetScheduler();
+            await sched.Start();
+
+            // define the job and tie it to our HelloJob class
+            IJobDetail job = JobBuilder.Create<HelloJob>()
+                .WithIdentity("myJob", "group1")
+                .Build();
+            
+            // Trigger the job to run now, and then every 40 seconds
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("myTrigger", "group1")
+                .StartNow()
+                .WithSimpleSchedule(x => x
+                    .WithIntervalInSeconds(1)
+                    .RepeatForever())
+            .Build();
+            await sched.ScheduleJob(job, trigger);
+            Console.ReadLine();
+        }
+    }
+
+    public class HelloJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            var redis = RedisManager.getRedisConn();
+            var db = redis.GetDatabase(11);
+            if (db.ListLength("Task." + "GIFT-ORDER-0-1") > 0)
+            {
+                RedisValue ids = await db.ListRightPopAsync("Task." + "GIFT-ORDER-0-1");
+                if (!ids.IsNull)
+                {
+                    Console.WriteLine(ids);
+                }
+                
+            }
+        }
+    }
+}
